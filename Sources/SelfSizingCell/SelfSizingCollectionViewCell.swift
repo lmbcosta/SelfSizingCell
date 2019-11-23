412e76f6e41eedc36b6e4f576b58fa035050aa99
Luís Costa <lmbcosta@hotmail.com>
2019-11-23 20:06:53 +0000

    Fixes some bugs

--- Sources/SelfSizingCell/SelfSizingCollectionViewCell.swift
+++ Sources/SelfSizingCell/SelfSizingCollectionViewCell.swift
@@ -41,7 +41,7 @@ open class SelfSizingCollectionViewCell: UICollectionViewCell {
         hasXib = false
         super.init(frame: frame)
         setupContentViewConstraints()
-        setupWidthConstraint()()
+        setupWidthConstraint()
     }
     
     public required init?(coder: NSCoder) {
